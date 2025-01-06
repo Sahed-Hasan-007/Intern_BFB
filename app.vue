5c15a0bdbee893b0147fda71704adadea708af22
p@ppu <skpappu34563456@gmail.com>
2025-01-06 23:49:37 +0600

    Adding file [6Jan25 11:50pm]

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-white max-w-screen-lg mx-auto text-center py-10 px-4">
+    <!-- Header Section -->
+    <header>
+        <img src="assets/logo1.png" alt="h_img" class="h-20 mx-auto">
+    </header>
+
+    <div class="mt-10">
+        <h1 class="text-green-800 text-3xl md:text-5xl font-bold px-5">
+            স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!<br> "আপনার স্টাইল, আপনার স্মার্টওয়াচ"
+        </h1>
+        <h2 class="bg-yellow-300 inline-block text-lg md:text-2xl font-medium mt-10 px-10 py-2">
+            আপনার জীবনকে আরও স্মার্ট করুন
+        </h2>
+    </div>
+
+    <div class="relative mt-10">
+        <img src="assets/main1.png" alt="main_img" class="w-3/4 mx-auto">
+        <div class="absolute inset-0 flex items-center justify-center">
+            <a href="https://www.geeksforgeeks.org/html-tutorial/" class="text-center">
+                <button class="bg-white bg-opacity-80 rounded-full w-24 h-24 flex items-center justify-center hover:bg-red-600 transition-all duration-300">
+                    <span class="text-red-600 hover:text-white text-4xl font-bold">▶</span>
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <div class="mt-10">
+         <button class="bg-green-800 text-yellow-300 text-lg md:text-xl font-bold py-3 px-6 rounded-full flex items-center mx-auto hover:bg-green-900 transition-all">
+            <span>অর্ডার করতে ক্লিক করুন</span>
+            <img src="assets/clic2.png" alt="clic_icon" class="h-6 w-6 ml-2">
+        </button>
+    </div>
+  </div>
+</template>
